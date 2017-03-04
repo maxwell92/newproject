8c62a21457a7ab7e920fad434daab939a64e5c09
maxwell92 <710028463@qq.com>
2017-03-04 23:44:32 +0800

    add organization and interface

--- src/components/mysql/controller.go
+++ src/components/mysql/controller.go
@@ -1,1 +1,35 @@
 package mysql
+
+import (
+	"components/mysql/resource/datacenters"
+	"components/mysql/resource/organizations"
+	"components/mysql/resource/users"
+)
+
+func AddControllers() {
+	users.New().AddControllers()
+	datacenters.New().AddControllers()
+	organizations.New().AddControllers()
+}
+
+func Organization() *organizations.Organization {
+	return &organizations.Organization{}
+}
+
+/*
+type IOrganization interface {
+	*QueryOrganizationInterface
+}
+
+type QueryOrganizationInterface interface {
+	Query()
+}
+
+func Organization() IOrganization {
+
+}
+*/
+
+func Datacenter() {
+
+}
